7c71aad0ca7c78a1b9be134f35ff710744446cc4
Guillaume Doumer <gdoumer@k2r3p6.42mulhouse.fr>
2024-03-07 12:51:43 +0100

    gd | 07/03-12:51 : Ajout de tous les .h necessaires

--- include/minishell.h
+++ include/minishell.h
@@ -1,4 +1,23 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/time.h>
+# include <sys/stat.h>
+# include <sys/resource.h>
+# include <signal.h>
+# include <dirent.h>
+# include <string.h>
+# include <errno.h>
+# include <sys/ioctl.h>
+# include <termios.h>
+# include <curses.h>
+# include <term.h>
+
 #endif
