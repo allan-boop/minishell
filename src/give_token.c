cf2201fd95f092dd5197d1d873fc82b52f87812e
Allan Hans <ahans@k0r2p7.42mulhouse.fr>
2024-03-26 14:01:04 +0100

    ah | 26/03-14:00 : add token chevron

--- src/give_token.c
+++ src/give_token.c
@@ -1,5 +1,34 @@
 #include "../include/minishell.h"
 
+static int	define_token_redirection(t_list_struct *node)
+{
+	if (node->content[0] == '>')
+	{
+		if (node->content[1] == '>')
+		{
+			if (node->content[2] != '\0')
+				return (0);
+			return (1);
+		}
+		if (node->content[1] != '\0')
+			return (0);
+		return (1);
+	}
+	else if (node->content[0] == '<')
+	{
+		if (node->content[1] == '<')
+		{
+			if (node->content[2] != '\0')
+				return (0);
+			return (1);
+		}
+		if (node->content[1] != '\0')
+			return (0);
+		return (1);
+	}
+	return (0);
+}
+
 static int	define_token_operator(t_list_struct *node)
 {
 	if (node->content[0] == '|')
