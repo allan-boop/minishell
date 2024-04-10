24a60b01f09c330236019a301709bac402b531d9
Guillaume Doumer <gdoumer@k2r3p4.42mulhouse.fr>
2024-04-10 13:12:58 +0200

    gd | 10/04-13:12 : creation export 1.111

--- src/pars/utils_export_quote.c
+++ src/pars/utils_export_quote.c
@@ -1,38 +1,9 @@
 #include "../../include/minishell.h"
 
-static int	ft_count_simple_quotes(char *arg, size_t simple_quote)
-{
-	size_t		i;
 
-	i = 0;
-	while (arg[i])
-	{
-		if (arg[i] == '\'')
-			simple_quote++;
-	}
-	return (simple_quote);
-}
-
-static int	ft_count_double_quotes(char *arg, size_t double_quote)
-{
-	size_t		i;
-
-	i = 0;
-	while (arg[i])
-	{
-		if (arg[i] == '\'')
-			double_quote++;
-	}
-	return (double_quote);
-}
 
 char	*ft_del_quotes(char *arg)
 {
-	size_t		simple_quote;
-	size_t		double_quote;
-
-	simple_quote = 0;
-	double_quote = 0;
-	simple_quote = ft_count_simple_quotes(arg, simple_quote);
-	double_quote = ft_count_double_quotes(arg, double_quote);
+	(void)arg;
+	return(NULL);
 }
