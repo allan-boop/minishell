4f199ddc7edaa094ebbad02270296809c98f7912
Guillaume Doumer <gdoumer@k2r3p4.42mulhouse.fr>
2024-04-10 17:16:34 +0200

    gd | 10/04-17:16 : creation export 1.113

--- src/exec/utils.c
+++ src/exec/utils.c
@@ -36,3 +36,58 @@ void	ft_replace_space_in_str(char *line, bool only_quote)
 		i++;
 	}
 }
+
+static void	ft_del_quote_export(char *line, int i)
+{
+	int	j;
+
+	j = i;
+	if (line[j] == 34)
+	{
+		while (line[j])
+		{
+			line[j] = line[j + 1];
+			j++;
+		}
+		line[j] = '\0';
+	}
+	if (line[j] == 39)
+	{
+		while (line[j])
+		{
+			line[j] = line[j + 1];
+			j++;
+		}
+		line[j] = '\0';
+	}
+
+}
+
+void	ft_replace_quote_export(char *line, bool only_quote)
+{
+	int		i;
+	char	quote;
+
+	i = 0;
+	while (line[i])
+	{
+		if (line[i] == 34 || line[i] == 39)
+		{
+			quote = line[i];
+			ft_del_quote_export(line, i);
+			if (line[i + 1])
+				i++;
+			while (line[i] && line[i] != quote)
+			{
+				if (only_quote == false && line[i] == ' ')
+					line[i] = 1;
+				i++;
+			}
+			if (line[i] && line[i] == quote && line[i] == 34)
+				line[i] = 2;
+			else if (line[i] && line[i] == quote && line[i] == 39)
+				line[i] = 3;
+		}
+		i++;
+	}
+}
--- src/pars/utils_export_quote.c
+++ src/pars/utils_export_quote.c
@@ -1,7 +1,39 @@
 #include "../../include/minishell.h"
 
-char	*ft_del_quotes(char *arg)
+static int	ft_check_name_quotes(char *arg)
 {
-	(void)arg;
-	return (NULL);
+	int		i;
+	char	*just_name_var;
+	char	quote;
+
+	if (!arg)
+		return (1);
+	i = 0;
+	quote = 0;
+	just_name_var = ft_find_name_var(arg);
+	while (just_name_var[i])
+	{
+		if (quote == 0 && (just_name_var[i] == '\''
+				|| just_name_var[i] == '\"'))
+			quote = just_name_var[i];
+		else if (quote == '\'' && just_name_var[i] == '\"')
+			return (syntax_error(QUOTE_FAIL), 1);
+		else if (quote == '\"' && just_name_var[i] == '\'')
+			return (syntax_error(QUOTE_FAIL), 1);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_del_quotes(char *arg, t_mini *shell)
+{
+	if (ft_check_name_quotes(arg) == 1)
+		return (1);
+	if (shell->tab_pars[shell->tab_index + 1] != NULL)
+	{
+		syntax_error(INVALID_IDENTIFIER);
+		return (1);
+	}
+	ft_replace_quote_export(arg, true);
+	return (0);
 }
