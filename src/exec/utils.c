e69bc8f3e467adae930e791711ca84c1b4e0235f
allan hans <80991295+allan-boop@users.noreply.github.com>
2024-04-15 16:32:07 +0200

    Update utils.c

--- src/exec/utils.c
+++ src/exec/utils.c
@@ -37,59 +37,78 @@ void	ft_replace_space_in_str(char *line, bool only_quote)
 	}
 }
 
+static int	rep_quote(char **line, int *i, char **tmp, t_str_utils *utils)
+{
+	char	quote;
+
+	if ((*line)[*i] == 34 || (*line)[*i] == 39)
+	{
+		quote = (*line)[*i];
+		utils->start = ++(*i);
+		while ((*line)[*i] && (*line)[*i] != quote)
+		{
+			if (!(*line)[*i])
+				return (syntax_error(QUOTE_FAIL), 1);
+			(*i)++;
+		}
+		utils->end = *i - 1;
+		if (!*tmp)
+			*tmp = ft_substr_shell((*line), utils->start,
+					(utils->end - utils->start) + 1);
+		else
+			*tmp = ft_strjoin_shell(*tmp,
+					ft_substr_shell((*line), utils->start,
+						(utils->end - utils->start) + 1));
+	}
+	else
+		*tmp = ft_strjoin_shell(*tmp, ft_substr_shell((*line), *i, 1));
+	(*i)++;
+	return (0);
+}
+
+static int	replace_quote(char **line, int *i, char **new_line)
+{
+	t_str_utils	utils;
+	char		*tmp;
+	int			ret;
+
+	tmp = NULL;
+	utils.start = 0;
+	utils.end = 0;
+	if ((*line)[*i] == 34 || (*line)[*i] == 39)
+	{
+		utils.start = *i;
+		*new_line = ft_substr_shell(*line, 0, utils.start);
+		while ((*line)[*i])
+		{
+			ret = rep_quote(line, i, &tmp, &utils);
+			if (ret != 0)
+				return (ret);
+		}
+		*new_line = ft_strjoin_shell(*new_line, tmp);
+		*new_line = ft_strjoin_shell(*new_line,
+				ft_substr_shell((*line), utils.end + 2, *i - utils.end));
+	}
+	else
+		(*i)++;
+	return (0);
+}
+
 int	ft_replace_quote_export(char **line)
 {
 	char	*new_line;
-	char	*tmp;
-	char	quote;
 	int		i;
-	size_t	start;
-	size_t	end;
+	int		ret;
 
 	if (!line)
 		return (1);
-	quote = 0;
-	start = 0;
-	end = 0;
 	i = 0;
 	new_line = NULL;
-	tmp = NULL;
-
 	while ((*line)[i])
 	{
-		if ((*line)[i] == 34 || (*line)[i] == 39)
-		{
-			start = i;
-			new_line = ft_substr_shell(*line, 0, start);
-			while ((*line)[i])
-			{
-				if ((*line)[i] == 34 || (*line)[i] == 39)
-				{
-					quote = (*line)[i];
-					start = ++i;
-					while ((*line)[i] && (*line)[i] != quote)
-					{
-						if (!(*line)[i])
-							return (syntax_error(QUOTE_FAIL),1);
-						i++;
-					}
-					end = i - 1;
-					if (!tmp)
-						tmp = ft_substr_shell((*line), start, (end - start) + 1);
-					else
-						tmp = ft_strjoin_shell(tmp, ft_substr_shell((*line), start, (end - start) + 1));
-				}
-				else
-				{
-					tmp = ft_strjoin_shell(tmp, ft_substr_shell((*line), i, 1));
-				}
-				i++;
-			}
-			new_line = ft_strjoin_shell(new_line, tmp);
-			new_line = ft_strjoin_shell(new_line, ft_substr_shell((*line), end + 2, i - end));
-		}
-		else
-			i++;
+		ret = replace_quote(line, &i, &new_line);
+		if (ret != 0)
+			return (ret);
 	}
 	if (new_line)
 		(*line) = ft_strdup_shell(new_line);
