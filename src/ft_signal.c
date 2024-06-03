61ca7a7a46b502234d91705ffb82e90edc7cf8dd
allan hans <allan.hans68350@gmail.com>
2024-06-03 13:51:25 +0200

    ah | 03/06-13:51 : ^C

--- src/ft_signal.c
+++ src/ft_signal.c
@@ -4,11 +4,7 @@ static void	signal_handler(int sig)
 {
 	if (sig == SIGINT)
 	{
-		write(1, "^C", 2);
 		write(1, "\n", 1);
-		rl_replace_line("", 0);
-		rl_on_new_line();
-		rl_redisplay();
 		signal(SIGINT, signal_handler);
 	}
 	else if (sig == SIGQUIT)
