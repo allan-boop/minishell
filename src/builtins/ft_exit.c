2023c3d05dd1a45289c15f543b9b0cecb5d25f22
allan hans <allan.hans68350@gmail.com>
2024-06-12 16:41:14 +0200

    ah | 12/06-16:41 : exit return value

--- src/builtins/ft_exit.c
+++ src/builtins/ft_exit.c
@@ -5,11 +5,13 @@ bool	ft_exit(char **envp, char ***copy_envp, t_mini **shell)
 	int		j;
 	char	*shlvl;
 	char	**tmp;
+	int		ret_val;
 
 	tmp = *copy_envp;
 	shlvl = ft_getenv("SHLVL", envp);
 	j = ft_atoi(shlvl);
 	j--;
+	ret_val = ft_atoi((*shell)->tab_pars[(*shell)->tab_index + 1]);
 	shlvl = ft_itoa(j);
 	ft_setenv_shell("SHLVL", shlvl, &envp);
 	ft_printf("exit\n");
@@ -23,6 +25,7 @@ bool	ft_exit(char **envp, char ***copy_envp, t_mini **shell)
 	close((*shell)->og_stdin);
 	close((*shell)->og_stdout);
 	ft_del_all();
-	exit(0);
+	ret_val = ret_val % 256;
+	exit(ret_val);
 	return (true);
 }
