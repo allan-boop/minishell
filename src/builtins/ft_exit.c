e4dcf10ec00e2c3c5131dee7e284896758e34917
allan hans <allan.hans68350@gmail.com>
2024-06-13 18:16:39 +0200

    ah | 13/06-18:16 : exit error

--- src/builtins/ft_exit.c
+++ src/builtins/ft_exit.c
@@ -1,5 +1,26 @@
 #include "../../include/minishell.h"
 
+static bool	error_exit(t_mini **shell, int i)
+{
+	if ((*shell)->tab_pars[(*shell)->tab_index + 1] != NULL
+		&& (*shell)->tab_pars[(*shell)->tab_index + 2] != NULL)
+		return (true);
+	if ((*shell)->tab_pars[(*shell)->tab_index + 1] != NULL)
+	{
+		while ((*shell)->tab_pars[(*shell)->tab_index + 1][i])
+		{
+			if (ft_isalpha((*shell)->tab_pars[(*shell)->tab_index + 1][i]))
+			{
+				ft_printf("exit: %s: numeric argument required\n",
+					(*shell)->tab_pars[(*shell)->tab_index + 1]);
+				exit(2);
+			}
+			i++;
+		}
+	}
+	return (false);
+}
+
 bool	ft_exit(char **envp, char ***copy_envp, t_mini **shell)
 {
 	int		j;
@@ -7,6 +28,8 @@ bool	ft_exit(char **envp, char ***copy_envp, t_mini **shell)
 	char	**tmp;
 	int		ret_val;
 
+	if (error_exit(shell, 0) == true)
+		return (syntax_error(MANY_ARGS));
 	tmp = *copy_envp;
 	shlvl = ft_getenv("SHLVL", envp);
 	j = ft_atoi(shlvl);
