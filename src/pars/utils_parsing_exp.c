b94467dac43678b906cea178ff50f923dc6c3fac
allan hans <allan.hans68350@gmail.com>
2024-06-26 18:05:43 +0200

    ah | 26/06-18:05 : echo $

--- src/pars/utils_parsing_exp.c
+++ src/pars/utils_parsing_exp.c
@@ -41,42 +41,83 @@ char	*print_exp_var(char *tab_pars, char *str, char *n_str,
 			t_int_utils *utils)
 {
 	int		k;
+	bool	quote;
 
+	quote = false;
 	while (str)
 	{
 		while (tab_pars[(*utils).j] && tab_pars[(*utils).j] != '$')
-			n_str[((*utils).i)++] = tab_pars[((*utils).j)++];
-		tab_pars[((*utils).j)++] = 'q';
-		str = clean_var(str);
-		(*utils).j = ft_find_exp(tab_pars, (*utils).j);
-		k = 0;
-		while ((*utils).envp[k])
 		{
-			if ((ft_strcmp("PWD", ft_find_name_var((*utils).envp[k])) == 0
-					&& (*utils).env->pwd == false)
-				|| (ft_strcmp("OLDPWD", ft_find_name_var((*utils).envp[k])) == 0
-					&& (*utils).env->oldpwd == false)
-				|| (ft_strcmp("HOME", ft_find_name_var((*utils).envp[k])) == 0
-					&& (*utils).env->home == false)
-				|| (ft_strcmp("SHLVL", ft_find_name_var((*utils).envp[k])) == 0
-					&& (*utils).env->shlvl == false)
-				|| (ft_strcmp("PATH", ft_find_name_var((*utils).envp[k])) == 0
-					&& (*utils).env->path == false))
+			if (tab_pars[(*utils).j] == 39)
 			{
+				n_str[((*utils).i)++] = tab_pars[((*utils).j++)];
+				while (tab_pars[(*utils).j] != 39)
+				{
+					if (tab_pars[(*utils).j] == '$')
+					{
+						n_str[((*utils).i)++] = tab_pars[((*utils).j)];
+						tab_pars[((*utils).j)++] = 'q';
+						quote = true;
+					}
+					else
+						n_str[((*utils).i)++] = tab_pars[((*utils).j)++];
+				}
+				n_str[((*utils).i)++] = tab_pars[((*utils).j)++];
+			}
+			else
+				n_str[((*utils).i)++] = tab_pars[((*utils).j)++];
+		}
+		if (quote == false)
+		{
+			tab_pars[((*utils).j)++] = 'q';
+			str = clean_var(str);
+			(*utils).j = ft_find_exp(tab_pars, (*utils).j);
+			k = 0;
+			while ((*utils).envp[k])
+			{
+				if ((ft_strcmp("PWD", ft_find_name_var((*utils).envp[k])) == 0
+						&& (*utils).env->pwd == false)
+					|| (ft_strcmp("OLDPWD", ft_find_name_var((*utils).envp[k])) == 0
+						&& (*utils).env->oldpwd == false)
+					|| (ft_strcmp("HOME", ft_find_name_var((*utils).envp[k])) == 0
+						&& (*utils).env->home == false)
+					|| (ft_strcmp("SHLVL", ft_find_name_var((*utils).envp[k])) == 0
+						&& (*utils).env->shlvl == false)
+					|| (ft_strcmp("PATH", ft_find_name_var((*utils).envp[k])) == 0
+						&& (*utils).env->path == false))
+				{
+					k++;
+					continue ;
+				}
+				if (ft_strcmp(str + 1, ft_find_name_var((*utils).envp[k])) == 0)
+				{
+					n_str = ft_strjoin_shell(n_str,
+							ft_getenv(str + 1, (*utils).envp));
+					(*utils).i = ft_strlen(n_str);
+				}
 				k++;
-				continue ;
 			}
-			if (ft_strcmp(str + 1, ft_find_name_var((*utils).envp[k])) == 0)
-			{
-				n_str = ft_strjoin_shell(n_str,
-						ft_getenv(str + 1, (*utils).envp));
-				(*utils).i = ft_strlen(n_str);
-			}
-			k++;
 		}
 		str = ft_strchr(tab_pars, '$');
 	}
 	return (n_str);
+}
+
+char	*ft_strjoin_char(char *s1, char c)
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	str = ft_calloc_shell(ft_strlen(s1) + 2, sizeof(char));
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = c;
+	str[i + 1] = '\0';
+	return (str);
 }
 
 char	*if_exp_var(t_mini *shell, t_env *env, int *i)
@@ -99,7 +140,11 @@ char	*if_exp_var(t_mini *shell, t_env *env, int *i)
 			n_str = print_exp_var(shell->tab_pars[*i], str, n_str, &utils);
 		}
 		while (shell->tab_pars[*i][utils.j])
-			n_str[(utils.i)++] = shell->tab_pars[*i][(utils.j)++];
+		{
+			n_str = ft_strjoin_char(n_str, shell->tab_pars[*i][utils.j]);
+			(utils.i)++;
+			(utils.j)++;
+		}
 		n_str[utils.i] = '\0';
 		return (n_str);
 	}
